ec4e7a6a03a534bdb05eecabb72fb93c977ec462
王堃荃 <natielongjingcha@qq.com>
2023-04-26 11:13:01 +0800

    init project

---
+++ readme.md
@@ -0,0 +1,2 @@
+# 常用工具包sdk
+### 详情地址区域划分：org/example/getAreaCode.java
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import static org.example.getAreaCode.evaluate;
+
+public class Main {
+    public static void main(String[] args) {
+        evaluate(args[0]);
+    }
+}
